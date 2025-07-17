d89fa291b1e2e61d6779e9f9483205bdfea5b586
YangKaiyun <a496764480@gmail.com>
2025-07-17 17:15:21 +0800

    other

--- Basic/point/point_type.h
+++ Basic/point/point_type.h
@@ -11,50 +11,56 @@
 #define POINT_TYPE_H
 #include <Eigen/Dense>
 #include "point.h"
+
+
 namespace basic {
 typedef OrientedPoint RobotPose;
 typedef std::vector<Point> RobotPath;
 typedef Eigen::Vector3d Color;
+
 struct RobotSpeed {
-  double vx{0};
-  double vy{0};
-  double w{0};
-  RobotSpeed() {}
-  RobotSpeed(double vx, double vy, double w) : vx(vx), vy(vy), w(w) {}
+    double vx{0};
+    double vy{0};
+    double w{0};
+    RobotSpeed() {}
+    RobotSpeed(double vx, double vy, double w) : vx(vx), vy(vy), w(w) {}
 };
 
 inline std::ostream &operator<<(std::ostream &os, const RobotSpeed &p) {
-  os << "x:" << p.vx << " y:" << p.vy << " theta:" << p.w;
-  return os;
+    os << "x:" << p.vx << " y:" << p.vy << " theta:" << p.w;
+    return os;
 }
+
+
 struct RobotState : public RobotPose, RobotSpeed {
- public:
-  RobotState() = default;
-  RobotState(const RobotPose &p, const RobotSpeed &s) : RobotPose(p), RobotSpeed(s) {}
-  RobotState(const OrientedPoint &p) : RobotPose(p) {}
+public:
+    RobotState() = default;
+    RobotState(const RobotPose &p, const RobotSpeed &s) : RobotPose(p), RobotSpeed(s) {}
+    RobotState(const OrientedPoint &p) : RobotPose(p) {}
 };
+
+
 // 激光点云数据结构
 struct LaserScan {
-  LaserScan() = default;
-  LaserScan(int i, std::vector<Point> d) : id(i), data(d) {}
-  int id;                   // 激光ID
-  std::vector<Point> data;  // 点坐标
-  void push_back(Point p) { data.push_back(p); }
-  void clear() { data.clear(); }
+    LaserScan() = default;
+    LaserScan(int i, std::vector<Point> d) : id(i), data(d) {}
+    int id;                   // 激光ID
+    std::vector<Point> data;  // 点坐标
+    void push_back(Point p) { data.push_back(p); }
+    void clear() { data.clear(); }
 };
-// 机器人路径 由点集组成
 
-inline Eigen::Vector3d absoluteSum(const Eigen::Vector3d &p,
-                                   const Eigen::Vector3d &d) {
-  Eigen::Quaterniond q1 = Eigen::AngleAxisd(p[2], Eigen::Vector3d::UnitZ()) *
-                          Eigen::AngleAxisd(0.0, Eigen::Vector3d::UnitY()) *
-                          Eigen::AngleAxisd(0.0, Eigen::Vector3d::UnitX());
-  Eigen::Vector3d t1 = Eigen::Vector3d(p[0], p[1], 0.0);
-  Eigen::Vector3d p1 = Eigen::Vector3d(d[0], d[1], 0.0);
-  Eigen::Vector3d pw;
-  pw = q1 * p1 + t1;
-  pw[2] += d[2];
-  return pw;
+// 机器人路径 由点集组成
+inline Eigen::Vector3d absoluteSum(const Eigen::Vector3d &p, const Eigen::Vector3d &d) {
+    Eigen::Quaterniond q1 = Eigen::AngleAxisd(p[2], Eigen::Vector3d::UnitZ()) *
+                            Eigen::AngleAxisd(0.0, Eigen::Vector3d::UnitY()) *
+                            Eigen::AngleAxisd(0.0, Eigen::Vector3d::UnitX());
+    Eigen::Vector3d t1 = Eigen::Vector3d(p[0], p[1], 0.0);
+    Eigen::Vector3d p1 = Eigen::Vector3d(d[0], d[1], 0.0);
+    Eigen::Vector3d pw;
+    pw = q1 * p1 + t1;
+    pw[2] += d[2];
+    return pw;
 }
 
 }  // namespace basic
